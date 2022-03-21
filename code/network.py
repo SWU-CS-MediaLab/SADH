6cf36a95ae85250e3941b4c101fd17c3172e1fab
JohnLearningMachineLearning <48585149+JohnLearningMachineLearning@users.noreply.github.com>
2022-03-21 17:08:42 +1100

    Update network.py

--- code/network.py
+++ code/network.py
@@ -2,37 +2,6 @@ import torch.nn as nn
 from torchvision import models
 from torch.nn import functional as F
 import torch
-class AlexNet(nn.Module):
-    def __init__(self, hash_bit, pretrained=True):
-        super(AlexNet, self).__init__()
-
-        model_alexnet = models.alexnet()
-        model_alexnet.load_state_dict(torch.load('/home/featurize/data/alexnet.pth'))
-        self.features = model_alexnet.features
-        cl1 = nn.Linear(256 * 6 * 6, 4096)
-        cl1.weight = model_alexnet.classifier[1].weight
-        cl1.bias = model_alexnet.classifier[1].bias
-
-        cl2 = nn.Linear(4096, 4096)
-        cl2.weight = model_alexnet.classifier[4].weight
-        cl2.bias = model_alexnet.classifier[4].bias
-
-        self.hash_layer = nn.Sequential(
-            nn.Dropout(),
-            cl1,
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            cl2,
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, hash_bit),
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), 256 * 6 * 6)
-        x = self.hash_layer(x)
-        return x
-
 
 resnet_dict = {"ResNet18": models.resnet18, "ResNet34": models.resnet34, "ResNet50": models.resnet50(),
                "ResNet101": models.resnet101, "ResNet152": models.resnet152}
